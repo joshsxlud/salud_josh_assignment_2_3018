2aa010bc6ea4d903997c533693f742da0840efd4
joshsxlud <jsalud2@academic.rrc.ca>
2025-09-30 15:53:28 -0500

    Feat: Tested health endpoint.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express, { Express } from "express";
 import morgan from "morgan";
-import healthRouter from "./api/v1/routes/healthRoute";
 
 const app: Express = express();
 
 app.use(morgan("combined"));
 
-app.use("/api/v1/health", healthRouter);
+app.get("/api/v1/health", (req, res) => {
+    res.json({
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0"       
+    });
+});
 
 export default app;
