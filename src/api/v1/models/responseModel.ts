0e524f65b406c367646ec01d1f29fe84eb7bf5eb
joshsxlud <jsalud2@academic.rrc.ca>
2025-10-14 16:04:41 -0500

    Created a model for error responses.

--- src/api/v1/models/responseModel.ts
+++ src/api/v1/models/responseModel.ts
@@ -1,3 +1,5 @@
+import { HTTP_STATUS } from "../../constants/httpConstants";
+
 export interface ApiResponse<T> {
     status: string;
     data?: T;
@@ -10,4 +12,10 @@ export const successResponse = <T>(data?: T, message?: string): ApiResponse<T> =
     status: "success",
     data,
     message,
+});
+
+export const errorResponse = <T>(message?: string, code: string): ApiResponse<T> => ({
+    status: "error",
+    error: message,
+    code
 });
